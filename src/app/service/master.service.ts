451e5451faea84d09ed0f2edea88269cc5fb0ba3
Barros313 <gbarros1337@gmail.com>
2024-10-24 21:55:27 -0300

    Added crud routes
    Signed-off-by: Barros313 <gbarros1337@gmail.com>

--- src/app/service/master.service.ts
+++ src/app/service/master.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ProductModel } from '../model/product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MasterService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  apiurl='api/products';
+
+  getAll() {
+    return this.http.get<ProductModel[]>(this.apiurl);
+  }
+
+  getProduct(id: number) {
+    return this.http.get<ProductModel>(this.apiurl + id);
+  }
+
+  createProduct(product: ProductModel) {
+    return this.http.post(this.apiurl, product);
+  }
+
+  updateProduct(product: ProductModel) {
+    return this.http.put(this.apiurl + product.id, product);
+  }
+
+  deleteProduct(id: number) {
+    return this.http.delete(this.apiurl + id);
+  }
 }
