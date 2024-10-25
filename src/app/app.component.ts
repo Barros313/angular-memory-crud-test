4865ceaa980299c732083166cd908f42d239460b
Barros313 <gbarros1337@gmail.com>
2024-10-24 22:06:00 -0300

    Implemented function to set product list in app components
    Signed-off-by: Barros313 <gbarros1337@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MasterService } from './service/master.service';
+import { ProductModel } from './model/product.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'test';
+export class AppComponent implements OnInit {
+  productList!: ProductModel[];
+
+  constructor(private service: MasterService) {
+
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented');
+  }
+
+  loadProductList() {
+    this.service.getAll().subscribe(item => {
+      this.productList = item;
+
+      console.log(this.productList);
+    });
+  }
 }
